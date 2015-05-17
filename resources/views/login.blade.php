3fdb567153e2521547ada20809d5f368b7f858df
PapaChiang <annie84080@gmail.com>
2015-05-17 11:49:41 +0800

    landing page - jumbotron

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,19 +27,19 @@
 </head>
 <body>
 	<nav class="navbar navbar-inverse">
-  	<div class="container-fluid">
-	    <div class="navbar-header">
-	     	<a class="navbar-brand" href="{{url('/')}}">RakudaPack</a>
+	  	<div class="container-fluid">
+		    <div class="navbar-header">
+		     	<a class="navbar-brand" href="{{url('/')}}">RakudaPack</a>
+			</div>
+		    <div>
+		    
+			    <ul class="nav navbar-nav navbar-right">
+			        <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> 登入</a></li>
+			        <li><a href="register"><span class="glyphicon glyphicon-user"></span> 快速註冊</a></li>
+			    </ul>
+		    </div>
 		</div>
-	    <div>
-	    
-		    <ul class="nav navbar-nav navbar-right">
-		        <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> 登入</a></li>
-		        <li><a href="register"><span class="glyphicon glyphicon-user"></span> 快速註冊</a></li>
-		    </ul>
-	    </div>
-	</div>
-</nav>
+	</nav>
 	
 
 
